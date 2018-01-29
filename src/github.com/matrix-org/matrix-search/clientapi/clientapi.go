3f90038849b04294549a98bc13cd5b5690cdb829
Michael Telatynski <7t3chguy@gmail.com>
2018-01-29 16:13:16 +0000

    another cleanup
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/github.com/matrix-org/matrix-search/clientapi/clientapi.go
+++ src/github.com/matrix-org/matrix-search/clientapi/clientapi.go
@@ -2,13 +2,14 @@ package clientapi
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
 	"github.com/blevesearch/bleve"
 	"github.com/blevesearch/bleve/search/query"
 	"github.com/fatih/set"
 	"github.com/gorilla/mux"
 	"github.com/matrix-org/gomatrix"
 	"github.com/matrix-org/matrix-search/indexing"
+	"io"
 	"net/http"
 	"strings"
 )
@@ -99,186 +100,192 @@ func generateQueryList(filterSet []string, fieldName string) []query.Query {
 	return nil
 }
 
-func RegisterHandler(router *mux.Router, idxr indexing.Indexer, hsURL, localpart, token string) {
-	router.HandleFunc("/clientapi/search/", func(w http.ResponseWriter, r *http.Request) {
-		var sr SearchRequest
-		if r.Body == nil {
-			http.Error(w, "Please send a request body", 400)
-			return
+func handler(body io.ReadCloser, idxr indexing.Indexer, hsURL, localpart, token string) (resp interface{}, err error) {
+	var sr SearchRequest
+	if body == nil {
+		err = errors.New("please send a request body")
+		return
+	}
+
+	err = json.NewDecoder(body).Decode(&sr)
+	if err != nil {
+		return
+	}
+
+	// TODO Should we use the Token given in the REQ then lookup their userid or WAT
+	cli, err := NewWrappedClient("@testguy:synapse", hsURL, localpart, token)
+	if err != nil {
+		return
+	}
+
+	joinedRooms, err := cli.joinedRooms()
+	if err != nil {
+		return
+	}
+
+	joinedRoomIDsSet := set.NewNonTS()
+	for i := range joinedRooms.JoinedRooms {
+		joinedRoomIDsSet.Add(joinedRooms.JoinedRooms[i])
+	}
+
+	q := sr.SearchCategories.RoomEvents
+
+	wantedRoomIDsSet := set.NewNonTS()
+	for i := range q.Filter.Rooms {
+		wantedRoomIDsSet.Add(q.Filter.Rooms[i])
+	}
+
+	roomIDsSet := set.Intersection(joinedRoomIDsSet, wantedRoomIDsSet)
+
+	for i := range q.Filter.NotRooms {
+		roomIDsSet.Remove(q.Filter.NotRooms[i])
+	}
+
+	qr := bleve.NewBooleanQuery()
+
+	// Must satisfy room_id
+	qr.AddMust(query.NewDisjunctionQuery(generateQueryList(set.StringSlice(roomIDsSet), "room_id")))
+
+	// Must satisfy sender
+	mustSenders := generateQueryList(q.Filter.Senders, "sender")
+	if len(mustSenders) > 0 {
+		qr.AddMust(query.NewDisjunctionQuery(mustSenders))
+	}
+
+	// Must satisfy not sender
+	qr.AddMustNot(generateQueryList(q.Filter.NotSenders, "sender")...)
+
+	// Must satisfy type
+	mustType := generateQueryList(q.Filter.Types, "type")
+	if len(mustType) > 0 {
+		qr.AddMust(query.NewDisjunctionQuery(mustType))
+	}
+
+	// Must satisfy not type
+	qr.AddMustNot(generateQueryList(q.Filter.NotTypes, "type")...)
+
+	// The user-entered query string
+	if len(q.Keys) > 0 {
+		oneOf := query.NewDisjunctionQuery(nil)
+		for _, key := range q.Keys {
+			qrs := query.NewMatchQuery(strings.ToLower(q.SearchTerm))
+			qrs.SetField(key)
+			oneOf.AddQuery(qrs)
 		}
-		err := json.NewDecoder(r.Body).Decode(&sr)
-		if err != nil {
-			http.Error(w, err.Error(), 400)
-			return
+		qr.AddMust(oneOf)
+	} else {
+		qr.AddMust(query.NewQueryStringQuery(strings.ToLower(q.SearchTerm)))
+	}
+
+	req := bleve.NewSearchRequest(qr)
+	res, err := idxr.Query(req)
+
+	if err != nil {
+		return
+	}
+
+	results := make([]Result, 0, len(res.Hits))
+	rooms := map[string]struct{}{}
+
+	wantsContext := q.EventContext != nil
+
+	beforeLimit := 5
+	afterLimit := 5
+	includeProfile := q.EventContext.IncludeProfile
+
+	if q.EventContext.BeforeLimit != nil {
+		beforeLimit = *q.EventContext.BeforeLimit
+	}
+	if q.EventContext.AfterLimit != nil {
+		afterLimit = *q.EventContext.AfterLimit
+	}
+
+	for _, hit := range res.Hits {
+
+		result := Result{
+			Rank: hit.Score,
 		}
 
-		q := sr.SearchCategories.RoomEvents
+		segs := strings.SplitN(hit.ID, "/", 2)
+		roomID := segs[0]
+		eventID := segs[1]
 
-		// Should we use the Token given in the REQ then lookup their userid or WAT
-		cli, err := NewWrappedClient("@testguy:synapse", hsURL, localpart, token)
-		if err != nil {
-			http.Error(w, err.Error(), 400)
-			return
-		}
-
-		resp, err := cli.joinedRooms()
-		if err != nil {
-			panic(err)
-		}
-
-		joinedRoomIDsSet := set.NewNonTS()
-		for i := range resp.JoinedRooms {
-			joinedRoomIDsSet.Add(resp.JoinedRooms[i])
-		}
-
-		wantedRoomIDsSet := set.NewNonTS()
-		for i := range q.Filter.Rooms {
-			wantedRoomIDsSet.Add(q.Filter.Rooms[i])
-		}
-
-		joinedRoomIDsSet.Add("!room1")
-		joinedRoomIDsSet.Add("!room2")
-
-		roomIDsSet := set.Intersection(joinedRoomIDsSet, wantedRoomIDsSet)
-
-		for i := range q.Filter.NotRooms {
-			roomIDsSet.Remove(q.Filter.NotRooms[i])
-		}
-
-		qr := bleve.NewBooleanQuery()
-
-		// Must satisfy room_id
-		qr.AddMust(query.NewDisjunctionQuery(generateQueryList(set.StringSlice(roomIDsSet), "room_id")))
-
-		// Must satisfy sender
-		mustSenders := generateQueryList(q.Filter.Senders, "sender")
-		if len(mustSenders) > 0 {
-			qr.AddMust(query.NewDisjunctionQuery(mustSenders))
-		}
-
-		// Must satisfy not sender
-		qr.AddMustNot(generateQueryList(q.Filter.NotSenders, "sender")...)
-
-		// Must satisfy type
-		mustType := generateQueryList(q.Filter.Types, "type")
-		if len(mustType) > 0 {
-			qr.AddMust(query.NewDisjunctionQuery(mustType))
-		}
-
-		// Must satisfy not type
-		qr.AddMustNot(generateQueryList(q.Filter.NotTypes, "type")...)
-
-		// The user-entered query string
-		if len(q.Keys) > 0 {
-			oneOf := query.NewDisjunctionQuery(nil)
-			for _, key := range q.Keys {
-				qrs := query.NewMatchQuery(strings.ToLower(q.SearchTerm))
-				qrs.SetField(key)
-				oneOf.AddQuery(qrs)
+		if wantsContext {
+			context, err := cli.resolveEvent(roomID, eventID, beforeLimit, afterLimit)
+			if err != nil {
+				return
 			}
-			qr.AddMust(oneOf)
-		} else {
-			qr.AddMust(query.NewQueryStringQuery(strings.ToLower(q.SearchTerm)))
-		}
 
-		req := bleve.NewSearchRequest(qr)
-		res, err := idxr.Query(req)
-
-		if err != nil {
-			fmt.Println(err)
-			http.Error(w, err.Error(), 400)
-			return
-			// TODO handle err
-		}
-
-		results := make([]Result, 0, len(res.Hits))
-		rooms := map[string]struct{}{}
-
-		wantsContext := q.EventContext != nil
-
-		beforeLimit := 5
-		afterLimit := 5
-		includeProfile := q.EventContext.IncludeProfile
-
-		if q.EventContext.BeforeLimit != nil {
-			beforeLimit = *q.EventContext.BeforeLimit
-		}
-		if q.EventContext.AfterLimit != nil {
-			afterLimit = *q.EventContext.AfterLimit
-		}
-
-		for _, hit := range res.Hits {
-
-			result := Result{
-				Rank: hit.Score,
+			result.Result = context.Event
+			result.Context = &EventContext{
+				Start:        context.Start,
+				End:          context.End,
+				EventsBefore: context.EventsBefore,
+				EventsAfter:  context.EventsAfter,
 			}
 
-			segs := strings.SplitN(hit.ID, "/", 2)
-			roomID := segs[0]
-			eventID := segs[1]
+			if includeProfile {
+				result.Context.ProfileInfo = make(map[string]*UserProfile)
+				for _, ev := range context.State {
+					if ev.Type == "m.room.member" {
+						userProfile := UserProfile{}
 
-			if wantsContext {
-				context, err := cli.resolveEvent(roomID, eventID, beforeLimit, afterLimit)
-				if err != nil {
-					panic(err)
-				}
-				result.Result = context.Event
-				result.Context = &EventContext{
-					Start:        context.Start,
-					End:          context.End,
-					EventsBefore: context.EventsBefore,
-					EventsAfter:  context.EventsAfter,
-				}
-
-				if includeProfile {
-					result.Context.ProfileInfo = make(map[string]*UserProfile)
-					for _, ev := range context.State {
-						if ev.Type == "m.room.member" {
-							userProfile := UserProfile{}
-
-							if str, ok := ev.Content["displayname"].(string); ok {
-								userProfile.DisplayName = str
-							}
-							if str, ok := ev.Content["avatar_url"].(string); ok {
-								userProfile.AvatarURL = str
-							}
-
-							result.Context.ProfileInfo[*ev.StateKey] = &userProfile
+						if str, ok := ev.Content["displayname"].(string); ok {
+							userProfile.DisplayName = str
 						}
+						if str, ok := ev.Content["avatar_url"].(string); ok {
+							userProfile.AvatarURL = str
+						}
+
+						result.Context.ProfileInfo[*ev.StateKey] = &userProfile
 					}
 				}
-			} else {
-				// TODO get event by itself, new API
 			}
-
-			results = append(results, result)
-			rooms[result.Result.RoomID] = struct{}{}
+		} else {
+			// TODO get event by itself, new API
 		}
 
-		roomStateMap := map[string][]*gomatrix.Event{}
-		if q.IncludeState {
-			// fetch state from server using API.
-			for roomID := range rooms {
-				state, err := cli.latestState(roomID)
-				if err != nil {
-					panic(err)
-				}
-				roomStateMap[roomID] = state
+		results = append(results, result)
+		rooms[result.Result.RoomID] = struct{}{}
+	}
+
+	roomStateMap := map[string][]*gomatrix.Event{}
+	if q.IncludeState {
+		// fetch state from server using API.
+		for roomID := range rooms {
+			state, err := cli.latestState(roomID)
+			if err != nil {
+				return
 			}
+			roomStateMap[roomID] = state
+		}
+	}
+
+	resp = Results{
+		Categories{
+			RoomEventResults{
+				Count:   int(res.Total),
+				Results: results,
+				State:   roomStateMap,
+				//Groups:,
+				//NextBatch:,
+			},
+		},
+	}
+	return
+}
+
+func RegisterHandler(router *mux.Router, idxr indexing.Indexer, hsURL, localpart, token string) {
+	router.HandleFunc("/clientapi/search/", func(w http.ResponseWriter, r *http.Request) {
+		resp, err := handler(r.Body, idxr, hsURL, localpart, token)
+
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+			return
 		}
 
 		//hits, err := json.Marshal(events)
-		hits, err := json.Marshal(Results{
-			Categories{
-				RoomEventResults{
-					Count:   int(res.Total),
-					Results: results,
-					State:   roomStateMap,
-					//Groups:,
-					//NextBatch:,
-				},
-			},
-		})
+		hits, err := json.Marshal(resp)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
